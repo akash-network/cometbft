88b22bde55ce2a6e8fac7cdfc5195fcb7933e2ba
Artur Troian <troian@users.noreply.github.com>
2025-07-14 15:59:24 -0500

    refactor(core/rpc): allow to add custom routes to rpc proxy
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>

--- rpc/core/routes.go
+++ rpc/core/routes.go
@@ -8,50 +8,57 @@ import (
 
 type RoutesMap map[string]*rpc.RPCFunc
 
-// Routes is a map of available routes.
+var Routes RoutesMap = RoutesMap{}
+
+// GetRoutes is a map of available routes.
 func (env *Environment) GetRoutes() RoutesMap {
-	return RoutesMap{
-		// subscribe/unsubscribe are reserved for websocket events.
-		"subscribe":       rpc.NewWSRPCFunc(env.Subscribe, "query"),
-		"unsubscribe":     rpc.NewWSRPCFunc(env.Unsubscribe, "query"),
-		"unsubscribe_all": rpc.NewWSRPCFunc(env.UnsubscribeAll, ""),
-
-		// info AP
-		"health":               rpc.NewRPCFunc(env.Health, ""),
-		"status":               rpc.NewRPCFunc(env.Status, ""),
-		"net_info":             rpc.NewRPCFunc(env.NetInfo, ""),
-		"blockchain":           rpc.NewRPCFunc(env.BlockchainInfo, "minHeight,maxHeight", rpc.Cacheable()),
-		"genesis":              rpc.NewRPCFunc(env.Genesis, "", rpc.Cacheable()),
-		"genesis_chunked":      rpc.NewRPCFunc(env.GenesisChunked, "chunk", rpc.Cacheable()),
-		"block":                rpc.NewRPCFunc(env.Block, "height", rpc.Cacheable("height")),
-		"block_by_hash":        rpc.NewRPCFunc(env.BlockByHash, "hash", rpc.Cacheable()),
-		"block_results":        rpc.NewRPCFunc(env.BlockResults, "height", rpc.Cacheable("height")),
-		"commit":               rpc.NewRPCFunc(env.Commit, "height", rpc.Cacheable("height")),
-		"header":               rpc.NewRPCFunc(env.Header, "height", rpc.Cacheable("height")),
-		"header_by_hash":       rpc.NewRPCFunc(env.HeaderByHash, "hash", rpc.Cacheable()),
-		"check_tx":             rpc.NewRPCFunc(env.CheckTx, "tx"),
-		"tx":                   rpc.NewRPCFunc(env.Tx, "hash,prove", rpc.Cacheable()),
-		"tx_search":            rpc.NewRPCFunc(env.TxSearch, "query,prove,page,per_page,order_by"),
-		"block_search":         rpc.NewRPCFunc(env.BlockSearch, "query,page,per_page,order_by"),
-		"validators":           rpc.NewRPCFunc(env.Validators, "height,page,per_page", rpc.Cacheable("height")),
-		"dump_consensus_state": rpc.NewRPCFunc(env.DumpConsensusState, ""),
-		"consensus_state":      rpc.NewRPCFunc(env.GetConsensusState, ""),
-		"consensus_params":     rpc.NewRPCFunc(env.ConsensusParams, "height", rpc.Cacheable("height")),
-		"unconfirmed_txs":      rpc.NewRPCFunc(env.UnconfirmedTxs, "limit"),
-		"num_unconfirmed_txs":  rpc.NewRPCFunc(env.NumUnconfirmedTxs, ""),
-
-		// tx broadcast API
-		"broadcast_tx_commit": rpc.NewRPCFunc(env.BroadcastTxCommit, "tx"),
-		"broadcast_tx_sync":   rpc.NewRPCFunc(env.BroadcastTxSync, "tx"),
-		"broadcast_tx_async":  rpc.NewRPCFunc(env.BroadcastTxAsync, "tx"),
-
-		// abci API
-		"abci_query": rpc.NewRPCFunc(env.ABCIQuery, "path,data,height,prove"),
-		"abci_info":  rpc.NewRPCFunc(env.ABCIInfo, "", rpc.Cacheable()),
-
-		// evidence API
-		"broadcast_evidence": rpc.NewRPCFunc(env.BroadcastEvidence, "evidence"),
+	routes := make(RoutesMap)
+	for name, route := range Routes {
+		routes[name] = route
 	}
+
+	// subscribe/unsubscribe are reserved for websocket events.
+	routes["subscribe"] = rpc.NewWSRPCFunc(env.Subscribe, "query")
+	routes["unsubscribe"] = rpc.NewWSRPCFunc(env.Unsubscribe, "query")
+	routes["unsubscribe_all"] = rpc.NewWSRPCFunc(env.UnsubscribeAll, "")
+
+	// info AP
+	routes["health"] = rpc.NewRPCFunc(env.Health, "")
+	routes["status"] = rpc.NewRPCFunc(env.Status, "")
+	routes["net_info"] = rpc.NewRPCFunc(env.NetInfo, "")
+	routes["blockchain"] = rpc.NewRPCFunc(env.BlockchainInfo, "minHeight,maxHeight", rpc.Cacheable())
+	routes["genesis"] = rpc.NewRPCFunc(env.Genesis, "", rpc.Cacheable())
+	routes["genesis_chunked"] = rpc.NewRPCFunc(env.GenesisChunked, "chunk", rpc.Cacheable())
+	routes["block"] = rpc.NewRPCFunc(env.Block, "height", rpc.Cacheable("height"))
+	routes["block_by_hash"] = rpc.NewRPCFunc(env.BlockByHash, "hash", rpc.Cacheable())
+	routes["block_results"] = rpc.NewRPCFunc(env.BlockResults, "height", rpc.Cacheable("height"))
+	routes["commit"] = rpc.NewRPCFunc(env.Commit, "height", rpc.Cacheable("height"))
+	routes["header"] = rpc.NewRPCFunc(env.Header, "height", rpc.Cacheable("height"))
+	routes["header_by_hash"] = rpc.NewRPCFunc(env.HeaderByHash, "hash", rpc.Cacheable())
+	routes["check_tx"] = rpc.NewRPCFunc(env.CheckTx, "tx")
+	routes["tx"] = rpc.NewRPCFunc(env.Tx, "hash,prove", rpc.Cacheable())
+	routes["tx_search"] = rpc.NewRPCFunc(env.TxSearch, "query,prove,page,per_page,order_by")
+	routes["block_search"] = rpc.NewRPCFunc(env.BlockSearch, "query,page,per_page,order_by")
+	routes["validators"] = rpc.NewRPCFunc(env.Validators, "height,page,per_page", rpc.Cacheable("height"))
+	routes["dump_consensus_state"] = rpc.NewRPCFunc(env.DumpConsensusState, "")
+	routes["consensus_state"] = rpc.NewRPCFunc(env.GetConsensusState, "")
+	routes["consensus_params"] = rpc.NewRPCFunc(env.ConsensusParams, "height", rpc.Cacheable("height"))
+	routes["unconfirmed_txs"] = rpc.NewRPCFunc(env.UnconfirmedTxs, "limit")
+	routes["num_unconfirmed_txs"] = rpc.NewRPCFunc(env.NumUnconfirmedTxs, "")
+
+	// tx broadcast API
+	routes["broadcast_tx_commit"] = rpc.NewRPCFunc(env.BroadcastTxCommit, "tx")
+	routes["broadcast_tx_sync"] = rpc.NewRPCFunc(env.BroadcastTxSync, "tx")
+	routes["broadcast_tx_async"] = rpc.NewRPCFunc(env.BroadcastTxAsync, "tx")
+
+	// abci API
+	routes["abci_query"] = rpc.NewRPCFunc(env.ABCIQuery, "path,data,height,prove")
+	routes["abci_info"] = rpc.NewRPCFunc(env.ABCIInfo, "", rpc.Cacheable())
+
+	// evidence API
+	routes["broadcast_evidence"] = rpc.NewRPCFunc(env.BroadcastEvidence, "evidence")
+
+	return routes
 }
 
 // AddUnsafeRoutes adds unsafe routes.
